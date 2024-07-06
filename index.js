2c0d634eabd6c45707376338edc0a2c82aafcd8d
Aleph-Phoebe <phbwangui47@gmail.com>
2024-07-06 20:26:45 +0300

    DONE

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+
+//Function to findMatch
+function findMatching(collection, name) {
+    return collection.filter(driver => driver.toLowerCase() == name.toLowerCase());
+  }
+  
+//Function to fuzzyMatch
+  function fuzzyMatch(collection, name) {
+    return collection.filter(driver => driver.charAt(0,2) == name.charAt(0,2));
+  }
+  
+//Function to matchName
+  function matchName(collection, name) {
+    return collection.filter(driver => driver.name == name);
+  }
+
